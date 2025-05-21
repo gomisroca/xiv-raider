f4c96ed45e89c7187341af33ab305b12739cbe90
Adrià Gomis <gomisroca@gmail.com>
2025-05-21 08:37:58 +0200

    chore: downgrade eslint to v8

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,15 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["prettier", "vitest-globals"],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  extends: [
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "project": true
+  },
+  "plugins": [
+    "@typescript-eslint",
+    "prettier",
+    "vitest-globals"
+  ],
+  "extends": [
     "next/core-web-vitals",
     "plugin:prettier/recommended",
     "plugin:@typescript-eslint/recommended-type-checked",
@@ -12,66 +17,33 @@ const config = {
     "plugin:vitest-globals/recommended",
     "plugin:storybook/recommended"
   ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    tsconfigRootDir: __dirname,
-    projectService: true,
-  },
-  overrides: [
-    // Template files don't have reliable type information
-    {
-      files: ["./cli/template/**/*.{ts,tsx}"],
-      extends: ["plugin:@typescript-eslint/disable-type-checked"],
-    },
-  ],
-  rules: {
-    // These off/not-configured-the-way-we-want lint rules we like & opt into
-    "@typescript-eslint/no-explicit-any": "error",
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-        destructuredArrayIgnorePattern: "^_",
-        caughtErrorsIgnorePattern: "^_",
-      },
-    ],
+  "rules": {
+    "prettier/prettier": "error",
+    "no-console": "warn",
+    "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
+        "prefer": "type-imports",
+        "fixStyle": "inline-type-imports"
+      }
     ],
-
-    // These lint rules don't make sense for us but are enabled in the preset configs
-    "@typescript-eslint/no-confusing-void-expression": "off",
-    "@typescript-eslint/restrict-template-expressions": "off",
-
-    // This rule doesn't seem to be working properly
-    "@typescript-eslint/prefer-nullish-coalescing": "off",
-
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
-   
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        "argsIgnorePattern": "^_"
+      }
+    ],
     "@typescript-eslint/require-await": "off",
     "@typescript-eslint/no-misused-promises": [
       "error",
       {
-        checksVoidReturn: {
-          attributes: false,
-        },
-      },
+        "checksVoidReturn": {
+          "attributes": false
+        }
+      }
     ],
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    
-    "prettier/prettier": "error",
-    eqeqeq: "error",
-    "no-console": "warn",
-    "no-undef": "off",
-  },
-};
-
+  }
+}
 module.exports = config;
