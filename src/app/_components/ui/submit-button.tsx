acadfdfcfa075a3a530465e97af1e5d7fab5e9c4
Adrià Gomis <gomisroca@gmail.com>
2025-05-22 14:51:19 +0200

    feat: reusable form submit button with useFormStatus

---
+++ src/app/_components/ui/submit-button.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+/**
+ * Button to submit a form. Uses `useFormStatus` hook to determine if the form is pending or not.
+ *
+ * @example
+ * <SubmitButton baseText="Submit" pendingText="Submitting..." />
+ */
+
+// Libraries
+import { useFormStatus } from 'react-dom';
+import { twMerge } from 'tailwind-merge';
+// Components
+import Button from '@/app/_components/ui/button';
+
+export default function SubmitButton({
+  baseText,
+  pendingText,
+  className,
+}: {
+  baseText: string;
+  pendingText: string;
+  className?: string;
+}) {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      type="submit"
+      name={baseText}
+      className={twMerge('w-fit font-semibold whitespace-nowrap', className)}
+      disabled={pending}>
+      {pending ? pendingText : baseText}
+    </Button>
+  );
+}
