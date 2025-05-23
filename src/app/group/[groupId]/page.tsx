cecc12c286910ddad2458fd37399d610a0eb3088
Adrià Gomis <gomisroca@gmail.com>
2025-05-23 14:01:22 +0200

    feat: character list in group view

--- src/app/group/[groupId]/page.tsx
+++ src/app/group/[groupId]/page.tsx
@@ -2,6 +2,8 @@ import { getGroup } from '@/server/queries/groups';
 import InviteButton from './invite-button';
 import { auth } from '@/server/auth';
 import NotAllowed from '@/app/_components/ui/not-allowed';
+import Link from '@/app/_components/ui/link';
+import { FaPencil } from 'react-icons/fa6';
 
 export default async function GroupPage({ params }: { params: Promise<{ groupId: string }> }) {
   const { groupId } = await params;
@@ -14,8 +16,29 @@ export default async function GroupPage({ params }: { params: Promise<{ groupId:
 
   return (
     <div className="flex flex-col items-center justify-center">
-      {group?.name}
-      <InviteButton groupId={groupId} />
+      <header className="flex items-center justify-between gap-2">
+        <h4 className="text-xl font-bold">{group?.name}</h4>
+        {!group.characters.find((character) => character.ownerId === session.user.id) && (
+          <Link href={`/group/${groupId}/create`} className="w-fit font-semibold uppercase">
+            Add Character
+          </Link>
+        )}
+        {session.user.id === group.createdBy.id && <InviteButton groupId={groupId} />}
+      </header>
+      <main className="flex flex-col gap-2 p-4">
+        {group.characters.map((character) => (
+          <div key={character.id} className="flex gap-1">
+            <Link href={`/group/${groupId}/${character.id}`} className="w-fit font-semibold uppercase">
+              {character.name}
+            </Link>
+            {session.user.id === group.createdBy.id && (
+              <Link href={`/group/${groupId}/${character.id}/update`} className="w-fit font-semibold uppercase">
+                <FaPencil />
+              </Link>
+            )}
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
