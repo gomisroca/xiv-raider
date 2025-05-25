2ac2f282e98c8effcd44797e00fac37ba2a096e4
Adrià Gomis <gomisroca@gmail.com>
2025-05-25 22:37:15 +0200

    style: add small sizing option

--- src/app/create-button.tsx
+++ src/app/create-button.tsx
@@ -5,8 +5,15 @@ import { redirect } from 'next/navigation';
 import Button from './_components/ui/button';
 import type { Session } from 'next-auth';
 import { GoPlus } from 'react-icons/go';
+import { twMerge } from 'tailwind-merge';
 
-export default function CreateButton({ session }: { session: Session | null }) {
+export default function CreateButton({
+  session,
+  size = 'normal',
+}: {
+  session: Session | null;
+  size?: 'normal' | 'small';
+}) {
   const handleCreate = async () => {
     if (!session && window.confirm('You must be logged in to create a group.')) await signIn('discord');
     else redirect('/create');
@@ -16,8 +23,16 @@ export default function CreateButton({ session }: { session: Session | null }) {
     <Button
       onClick={handleCreate}
       name="Create group"
-      className="group h-44 w-full max-w-xl text-2xl font-semibold uppercase md:h-86 md:text-4xl">
-      <GoPlus className="pointer-events-none h-72 w-72 transition duration-200 ease-in-out group-active:scale-75" />
+      className={twMerge(
+        'group w-full max-w-xl text-2xl font-semibold uppercase md:h-86 md:text-4xl',
+        size === 'small' ? 'h-16 w-20 md:h-20' : 'h-44'
+      )}>
+      <GoPlus
+        className={twMerge(
+          'pointer-events-none h-72 w-72 transition duration-200 ease-in-out group-active:scale-75',
+          size === 'small' ? 'h-20 w-20' : 'h-72 w-72'
+        )}
+      />
     </Button>
   );
 }
