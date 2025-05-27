e34677d24bfdd1e171f4e1302e5b6a3f35ef2ec1
Adrià Gomis <gomisroca@gmail.com>
2025-05-27 15:11:42 +0200

    feat: add boss loot map

--- src/utils/enums.tsx
+++ src/utils/enums.tsx
@@ -1,3 +1,4 @@
+import { GearSlot, GearStatus } from 'generated/prisma';
 import type { ReactElement } from 'react';
 import {
   GiVisoredHelm,
@@ -25,6 +26,26 @@ export const GearSlots = [
   'Ring1',
   'Ring2',
 ] as const;
+export const GearSlotIcons: Record<GearSlot, ReactElement> = {
+  Weapon: <GiBroadsword size={20} />,
+  Head: <GiVisoredHelm size={20} />,
+  Body: <GiArmorVest size={20} />,
+  Gloves: <GiGloves size={20} />,
+  Legs: <GiLegArmor size={20} />,
+  Feet: <GiBoots size={20} />,
+  Earrings: <GiEarrings size={20} />,
+  Necklace: <GiNecklace size={20} />,
+  Bracelet: <GiBracer size={20} />,
+  Ring1: <GiRing size={20} />,
+  Ring2: <GiRing size={20} />,
+};
+
+export const GearStatuses = ['BiS', 'NeedsTomestone', 'NeedsRaidDrop'] as const;
+export const GearStatusLabels: Record<GearStatus, string> = {
+  BiS: 'Best in Slot',
+  NeedsTomestone: 'Tomestone',
+  NeedsRaidDrop: 'Raid Drop',
+};
 
 export const Jobs = [
   'WAR',
@@ -50,29 +71,114 @@ export const Jobs = [
   'RDM',
 ] as const;
 
-export const GearStatuses = ['BiS', 'NeedsTomestone', 'NeedsRaidDrop'] as const;
-
 export const PlanPriorities = ['Melee', 'Ranged', 'Tank', 'Healer'] as const;
-
-export const GearSlotIcons: Record<GearSlot, ReactElement> = {
-  Weapon: <GiBroadsword size={20} />,
-  Head: <GiVisoredHelm size={20} />,
-  Body: <GiArmorVest size={20} />,
-  Gloves: <GiGloves size={20} />,
-  Legs: <GiLegArmor size={20} />,
-  Feet: <GiBoots size={20} />,
-  Earrings: <GiEarrings size={20} />,
-  Necklace: <GiNecklace size={20} />,
-  Bracelet: <GiBracer size={20} />,
-  Ring1: <GiRing size={20} />,
-  Ring2: <GiRing size={20} />,
+export const PlanPriorityJobs: Record<PlanPriority, Job[]> = {
+  Melee: ['MNK', 'SAM', 'RPR', 'DRG', 'NIN', 'VPR'],
+  Ranged: ['BRD', 'MCH', 'DNC', 'BLM', 'PCT', 'SMN', 'RDM'],
+  Tank: ['WAR', 'PLD', 'DRK', 'GNB'],
+  Healer: ['WHM', 'SCH', 'AST', 'SGE'],
 };
 
-export const GearStatusLabels: Record<GearStatus, string> = {
-  BiS: 'Best in Slot',
-  NeedsTomestone: 'Need Tomestone',
-  NeedsRaidDrop: 'Need Raid Drop',
+export const BossLootMap: Record<string, { slot: GearSlot; status: GearStatus }[]> = {
+  'Dancing Green': [
+    {
+      slot: GearSlot.Earrings,
+      status: GearStatus.NeedsRaidDrop,
+    },
+    {
+      slot: GearSlot.Necklace,
+      status: GearStatus.NeedsRaidDrop,
+    },
+    {
+      slot: GearSlot.Bracelet,
+      status: GearStatus.NeedsRaidDrop,
+    },
+    {
+      slot: GearSlot.Ring1,
+      status: GearStatus.NeedsRaidDrop,
+    },
+    {
+      slot: GearSlot.Ring2,
+      status: GearStatus.NeedsRaidDrop,
+    },
+  ],
+  'Sugar Riot': [
+    {
+      slot: GearSlot.Feet,
+      status: GearStatus.NeedsRaidDrop,
+    },
+    {
+      slot: GearSlot.Gloves,
+      status: GearStatus.NeedsRaidDrop,
+    },
+    {
+      slot: GearSlot.Head,
+      status: GearStatus.NeedsRaidDrop,
+    },
+    {
+      slot: GearSlot.Earrings,
+      status: GearStatus.NeedsTomestone,
+    },
+    {
+      slot: GearSlot.Necklace,
+      status: GearStatus.NeedsTomestone,
+    },
+    {
+      slot: GearSlot.Bracelet,
+      status: GearStatus.NeedsTomestone,
+    },
+    {
+      slot: GearSlot.Ring1,
+      status: GearStatus.NeedsTomestone,
+    },
+    {
+      slot: GearSlot.Ring2,
+      status: GearStatus.NeedsTomestone,
+    },
+  ],
+  'Brute Abombinator': [
+    {
+      slot: GearSlot.Body,
+      status: GearStatus.NeedsRaidDrop,
+    },
+    {
+      slot: GearSlot.Legs,
+      status: GearStatus.NeedsRaidDrop,
+    },
+    {
+      slot: GearSlot.Head,
+      status: GearStatus.NeedsTomestone,
+    },
+    {
+      slot: GearSlot.Body,
+      status: GearStatus.NeedsTomestone,
+    },
+    {
+      slot: GearSlot.Gloves,
+      status: GearStatus.NeedsTomestone,
+    },
+    {
+      slot: GearSlot.Legs,
+      status: GearStatus.NeedsTomestone,
+    },
+    {
+      slot: GearSlot.Feet,
+      status: GearStatus.NeedsTomestone,
+    },
+    {
+      slot: GearSlot.Weapon,
+      status: GearStatus.NeedsTomestone,
+    },
+  ],
+  'Howling Blade': [
+    {
+      slot: GearSlot.Weapon,
+      status: GearStatus.NeedsRaidDrop,
+    },
+  ],
 };
 
-export type GearSlot = (typeof GearSlots)[number];
-export type GearStatus = (typeof GearStatuses)[number];
+export type Job = (typeof Jobs)[number];
+export type PlanPriority = (typeof PlanPriorities)[number];
+export type GearSlots = (typeof GearSlots)[number];
+export type GearStatuses = (typeof GearStatuses)[number];
