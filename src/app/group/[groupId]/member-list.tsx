b86716027e2abe17e741a2612657dfb650de6798
Adrià Gomis <gomisroca@gmail.com>
2025-05-29 09:25:13 +0200

    feat: reimplement priority-based member list

---
+++ src/app/group/[groupId]/member-list.tsx
@@ -0,0 +1,68 @@
+import { JobIcon } from '@/app/_components/ui/icons';
+import Link from '@/app/_components/ui/link';
+import { PlanPriorityJobs, type PlanPriority } from '@/utils/enums';
+import { type Prisma, type GroupPlan } from 'generated/prisma';
+import { type Session } from 'next-auth';
+import { FaPencil } from 'react-icons/fa6';
+
+type ExtendedGroup = Prisma.GroupGetPayload<{
+  include: {
+    createdBy: true;
+    members: true;
+    characters: {
+      include: { owner: true; gear: true };
+    };
+  };
+}>;
+
+export default async function MemberList({
+  group,
+  plan,
+  session,
+}: {
+  group: ExtendedGroup;
+  plan: GroupPlan;
+  session: Session | null;
+}) {
+  const priorities: PlanPriority[] = [plan.priority_1, plan.priority_2, plan.priority_3, plan.priority_4];
+
+  // Create a map of priorities to characters
+  const charactersByPriority = priorities.map((priority) => {
+    const jobs = PlanPriorityJobs[priority];
+    const chararacters = group.characters.filter((char) => jobs.includes(char.job)); // cast if needed
+    return { priority, chararacters };
+  });
+
+  const characterOwner = session?.user.id === group.createdBy.id;
+
+  return (
+    <div className="flex items-center justify-center gap-4">
+      {charactersByPriority.map(({ priority, chararacters }, idx) => (
+        <div key={priority} className="text-sm">
+          <p className="font-medium">
+            Priority #{idx + 1}: <span className="text-lg font-semibold">{priority}</span>
+          </p>
+          <ul className="flex list-none flex-col gap-1">
+            {chararacters.map((character) => (
+              <li key={character.id} className="flex w-full items-center justify-start">
+                <Link
+                  href={`/group/${group.id}/${character.id}`}
+                  className={`flex flex-1 items-center justify-start gap-1 font-semibold uppercase ${characterOwner && 'rounded-r-none'}`}>
+                  <JobIcon job={character.job} />
+                  <h6>{character.name}</h6>
+                </Link>
+                {characterOwner && (
+                  <Link
+                    href={`/group/${group.id}/${character.id}/update`}
+                    className="rounded-l-none border-l-1 border-zinc-400 font-semibold uppercase">
+                    <FaPencil />
+                  </Link>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+}
--- src/app/group/[groupId]/page.tsx
+++ src/app/group/[groupId]/page.tsx
@@ -3,8 +3,9 @@ import InviteButton from './invite-button';
 import { auth } from '@/server/auth';
 import NotAllowed from '@/app/_components/ui/not-allowed';
 import Link from '@/app/_components/ui/link';
-import { FaPencil } from 'react-icons/fa6';
-import PlanWidget from './(planner)/plan-widget';
+import { getPlan } from '@/server/queries/plans';
+import BossLootList from './boss-list';
+import MemberList from './member-list';
 
 export default async function GroupPage({ params }: { params: Promise<{ groupId: string }> }) {
   const { groupId } = await params;
@@ -12,41 +13,34 @@ export default async function GroupPage({ params }: { params: Promise<{ groupId:
   const session = await auth();
 
   const group = await getGroup(groupId);
+  const plan = await getPlan(group.id);
+  if (!group || !plan) return null;
 
   if (!session || !group?.members.find((member) => member.id === session.user.id)) return <NotAllowed />;
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <header className="flex items-center justify-between gap-2">
-        <h4 className="text-xl font-bold">{group?.name}</h4>
-        {!group.characters.find((character) => character.ownerId === session.user.id) && (
-          <Link href={`/group/${groupId}/create`} className="w-fit font-semibold uppercase">
-            Add Character
-          </Link>
-        )}
-        {session.user.id === group.createdBy.id && (
-          <div className="flex items-center gap-2">
-            <Link href={`/group/${groupId}/plan`} className="w-fit font-semibold uppercase">
-              Update Plan
+      <header className="flex w-full items-center justify-between gap-2">
+        <h4 className="text-2xl font-bold">{group?.name}</h4>
+        <aside className="flex items-center gap-2">
+          {!group.characters.find((character) => character.ownerId === session.user.id) && (
+            <Link href={`/group/${groupId}/create`} className="w-fit font-semibold uppercase">
+              Add Character
             </Link>
-            <InviteButton groupId={groupId} />
-          </div>
-        )}
+          )}
+          {session.user.id === group.createdBy.id && (
+            <div className="flex items-center gap-2">
+              <Link href={`/group/${groupId}/plan`} className="w-fit font-semibold uppercase">
+                Update Plan
+              </Link>
+              <InviteButton groupId={groupId} />
+            </div>
+          )}
+        </aside>
       </header>
       <main className="flex flex-col gap-2 p-4">
-        <PlanWidget group={group} />
-        {group.characters.map((character) => (
-          <div key={character.id} className="flex gap-1">
-            <Link href={`/group/${groupId}/${character.id}`} className="w-fit font-semibold uppercase">
-              {character.name} - {character.owner.name}
-            </Link>
-            {session.user.id === group.createdBy.id && (
-              <Link href={`/group/${groupId}/${character.id}/update`} className="w-fit font-semibold uppercase">
-                <FaPencil />
-              </Link>
-            )}
-          </div>
-        ))}
+        <BossLootList characters={group.characters} plan={plan} />
+        <MemberList group={group} plan={plan} session={session} />
       </main>
     </div>
   );
