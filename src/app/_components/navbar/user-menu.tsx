4a9db4a111f31a5e910dab98f014b55ff1d1af98
Adrià Gomis <gomisroca@gmail.com>
2025-05-30 11:22:53 +0200

    fix: better-suited icon for logout button

--- src/app/_components/navbar/user-menu.tsx
+++ src/app/_components/navbar/user-menu.tsx
@@ -3,14 +3,15 @@
 import { type Session } from 'next-auth';
 import { signIn, signOut } from 'next-auth/react';
 import Button from '@/app/_components/ui/button';
-import { FaKey, FaUser } from 'react-icons/fa6';
+import { FaKey } from 'react-icons/fa6';
+import { MdExitToApp } from 'react-icons/md';
 
 export default function UserMenu({ session }: { session: Session | null }) {
   return (
     <>
       {session ? (
         <Button onClick={() => signOut()} name="User menu">
-          <FaUser size={20} />
+          <MdExitToApp size={20} />
         </Button>
       ) : (
         <Button onClick={() => signIn('discord')} name="Log in">
