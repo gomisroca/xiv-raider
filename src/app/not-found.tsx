d792295533a5b082be9af78425a584d44e2fcd10
Adrià Gomis <gomisroca@gmail.com>
2025-06-02 10:01:25 +0200

    style: improve go back button

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,8 +5,8 @@ export default function NotFound() {
     <div className="flex flex-col items-center justify-center gap-2">
       <h2 className="text-4xl font-semibold tracking-widest uppercase">404 | Not Found</h2>
       <p>We could not find the page you are looking for.</p>
-      <Link href="/" name="Home">
-        Return Home
+      <Link href="/" name="Home" className="w-fit font-semibold uppercase">
+        Go Back
       </Link>
     </div>
   );
