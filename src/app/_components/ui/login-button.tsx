d83ab24479ab8b8a35b821dc6eb4c95da4cb8803
Adrià Gomis <gomisroca@gmail.com>
2025-06-04 13:43:17 +0200

    feat: compartmentalize login button

---
+++ src/app/_components/ui/login-button.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { signIn } from 'next-auth/react';
+import Button from './button';
+import { twMerge } from 'tailwind-merge';
+import { FaKey } from 'react-icons/fa6';
+
+export default function LoginButton({ size = 'normal' }: { size?: 'normal' | 'large' }) {
+  return (
+    <Button
+      name="Login"
+      onClick={() => signIn('discord')}
+      className={twMerge(
+        'mx-auto gap-2 px-4 font-semibold uppercase',
+        size === 'large' ? 'h-14 w-2/3 text-4xl md:h-20' : 'w-fit'
+      )}>
+      <FaKey size={30} />
+      <span>Login</span>
+    </Button>
+  );
+}
--- src/app/_components/ui/not-allowed.tsx
+++ src/app/_components/ui/not-allowed.tsx
@@ -1,13 +1,10 @@
-import Button from './button';
-import { signIn } from '@/server/auth';
+import LoginButton from './login-button';
 
 export default async function NotAllowed() {
   return (
     <div className="flex flex-col gap-4">
       <p>You need to be logged in to access this page.</p>
-      <Button name="Login" onClick={() => signIn('discord')} className="mx-auto w-fit px-4 font-semibold uppercase">
-        Login
-      </Button>
+      <LoginButton />
     </div>
   );
 }
