7ade4233ab4092e38a89b253a389a61f38eeeff0
Adrià Gomis <gomisroca@gmail.com>
2025-06-04 13:43:52 +0200

    style: add slight shadow to navbar and footer btns

--- src/app/_components/navbar/index.tsx
+++ src/app/_components/navbar/index.tsx
@@ -13,7 +13,7 @@ export default async function Navbar() {
   return (
     <nav className="fixed top-0 right-0 z-10 flex h-12 w-fit items-center justify-end space-x-2 px-4 transition ease-in">
       {/* Link to the home page */}
-      <Link name="Home" href="/">
+      <Link name="Home" href="/" className="shadow-md">
         <MdHome size={20} />
       </Link>
       <UserMenu session={session} />
