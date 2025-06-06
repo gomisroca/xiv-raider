f5abe2e0ad529b6466ba22697b09427f0c3865a6
Adrià Gomis <gomisroca@gmail.com>
2025-06-06 12:22:42 +0200

    style: adjust sizing of several icons

--- src/app/_components/ui/icons.tsx
+++ src/app/_components/ui/icons.tsx
@@ -2,13 +2,21 @@ import Image from 'next/image';
 import { Job, LootType } from 'generated/prisma';
 
 export function JobIcon({ job }: { job: Job }) {
-  return <Image alt={job} src={`/jobs/${job}.png`} width="30" height="30" />;
+  return (
+    <Image
+      className="h-[24px] w-[24px] md:h-[30px] md:w-[30px]"
+      alt={job}
+      src={`/jobs/${job}.png`}
+      width="30"
+      height="30"
+    />
+  );
 }
 
 export function GearIcon({ gearSlot }: { gearSlot: string }) {
   if (gearSlot === 'Ring1' || gearSlot === 'Ring2') gearSlot = 'Ring';
   return (
-    <div className="flex h-[30px] w-[30px] items-center justify-center rounded-lg bg-zinc-100 dark:bg-zinc-900">
+    <div className="flex h-[20px] w-[20px] items-center justify-center rounded-lg bg-zinc-100 md:h-[30px] md:w-[30px] dark:bg-zinc-900">
       <Image
         alt={gearSlot}
         src={`/items/${gearSlot}.png`}
@@ -23,6 +31,7 @@ export function GearIcon({ gearSlot }: { gearSlot: string }) {
 export function LootIcon({ lootType }: { lootType: LootType }) {
   return (
     <Image
+      className="h-[20px] w-[20px] md:h-[30px] md:w-[30px]"
       alt={lootType === 'RaidDrop' ? 'chest' : 'tomestone'}
       src={`/items/${lootType === 'RaidDrop' ? 'chest' : 'tomestone'}.png`}
       width="30"
