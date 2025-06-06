288aaffc37fd5fac17cad720f6a564b56abf8d7b
Adrià Gomis <gomisroca@gmail.com>
2025-06-06 12:23:04 +0200

    style: add backdrop blur to navbar buttons

--- src/app/_components/navbar/index.tsx
+++ src/app/_components/navbar/index.tsx
@@ -11,7 +11,7 @@ import UserMenu from './user-menu';
 export default async function Navbar() {
   const session = await auth();
   return (
-    <nav className="fixed top-0 right-0 z-10 flex h-12 w-fit items-center justify-end space-x-2 px-4 transition ease-in">
+    <nav className="fixed top-0 right-0 z-10 flex h-12 w-fit items-center justify-end space-x-2 rounded-lg px-2 backdrop-blur-xs transition ease-in md:backdrop-blur-none">
       {/* Link to the home page */}
       <Link name="Home" href="/" className="shadow-md">
         <MdHome size={20} />
